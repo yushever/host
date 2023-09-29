e0e7fb89d5998a99c905a8c6f80396aab68e629e
yushever <iushina.tatiana@gmail.com>
2023-09-29 18:28:46 +0400

    card layout

--- components/CardList/CardList.tsx
+++ components/CardList/CardList.tsx
@@ -4,7 +4,14 @@ import { Grid, GridItem } from "@chakra-ui/react";
 
 export default function CardList() {
   return (
-    <Grid templateColumns="repeat(3, 2fr)" gap={6}>
+    <Grid
+      templateColumns={{
+        base: "repeat(3, 2fr)",
+        md: "repeat(4, 2fr)",
+        sm: "repeat(1, 1fr)",
+      }}
+      gap={6}
+      justifyItems="center">
       <CardAd />
       <CardAd />
       <CardAd />
